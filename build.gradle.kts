f912b6704912b0a9afbe3071a3bdf1d4015a8f13
McCabe Nickerson <mnickerson@vmware.com>
2022-11-04 20:44:13 -0600

    remove h2 driver

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,6 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.postgresql:postgresql")
-	implementation("com.h2database:h2")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("com.ninja-squad:springmockk:3.1.1")
